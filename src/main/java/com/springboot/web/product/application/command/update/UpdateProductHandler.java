b6c9ff6e4fde5cd3a457650268289890d595ce3d
Chuckytin <angeltorcalcatalan@gmail.com>
2026-05-18 09:32:24 +0200

    transactional rollbackfor añadido

--- src/main/java/com/springboot/web/product/application/command/update/UpdateProductHandler.java
+++ src/main/java/com/springboot/web/product/application/command/update/UpdateProductHandler.java
@@ -11,6 +11,7 @@ import com.springboot.web.productdetail.domain.entity.ProductDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
@@ -22,6 +23,7 @@ import java.util.ArrayList;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(rollbackFor = Exception.class)
 public class UpdateProductHandler implements RequestHandler<UpdateProductRequest, Void> {
 
     private final ProductRepository productRepository;
